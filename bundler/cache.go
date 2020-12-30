9534c28053bf8791776ea8e7729c38bdf5e3c805
Andrey Mikheev <mixe.andrey@gmail.com>
2020-12-30 09:18:16 +0200

    cache storage

--- bundler/cache.go
+++ bundler/cache.go
@@ -1,17 +1,14 @@
 package bundler
 
 import (
-	"bitbucket.org/HeilaSystems/cacheStorage"
-	"bitbucket.org/HeilaSystems/cacheStorage/mock"
+	"bitbucket.org/HeilaSystems/cacheStorage/mongodb"
 	"bitbucket.org/HeilaSystems/dependencybundler/constructors/cache"
 	"go.uber.org/fx"
 )
 
 func CacheFxOption() fx.Option {
 	return fx.Options(
-		fx.Provide(func() cacheStorage.CacheStorageBuilder{
-			return mock.Builder()
-		}),
+		fx.Provide(mongodb.NewMongoDbCacheStorage),
 		fx.Provide(cache.DefaultCacheStorageClient),
 	)
 }
