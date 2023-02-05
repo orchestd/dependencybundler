811762ddeae4f6149bef78ca93ae62f9a1a5299a
idan <idan.b@heilasystems.com>
2023-02-05 18:16:50 +0200

    fixed

--- depBundler/middlewares/trace/client.go
+++ depBundler/middlewares/trace/client.go
@@ -3,6 +3,7 @@ package trace
 import (
 	"context"
 	"fmt"
+	"github.com/opentracing/opentracing-go"
 	"github.com/opentracing/opentracing-go/ext"
 	"github.com/orchestd/transport/client"
 	"net/http"
