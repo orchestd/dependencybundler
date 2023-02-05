af99f9e8b483887e3abd4f95c985f84d1405bf11
idan <idan.b@heilasystems.com>
2023-02-05 13:51:07 +0200

    fixed import

--- bundler/monitoring.go
+++ bundler/monitoring.go
@@ -2,10 +2,10 @@ package bundler
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/orchestd/bprometheus"
 	"github.com/orchestd/dependencybundler/constructors/monitoring"
 	"github.com/orchestd/dependencybundler/interfaces/configuration"
 	monitoring2 "github.com/orchestd/monitoring"
+	"github.com/orchestd/monitoring/bprometheus"
 	"github.com/orchestd/sharedlib/consts"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"go.uber.org/fx"
