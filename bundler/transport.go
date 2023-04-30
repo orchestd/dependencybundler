86314936cfd85e3fdfc66abf467cf88147800437
andrey <andrey.m@heilasystems.com>
2023-04-30 17:41:37 +0300

    added RunMethodWithTrace

--- bundler/transport.go
+++ bundler/transport.go
@@ -1,14 +1,18 @@
 package bundler
 
 import (
+	"context"
+	"github.com/opentracing/opentracing-go"
+	"github.com/opentracing/opentracing-go/ext"
 	dspConstructor "github.com/orchestd/dependencybundler/constructors/discoveryService"
 	transportConstructor "github.com/orchestd/dependencybundler/constructors/transport"
 	clientMiddlewares "github.com/orchestd/dependencybundler/constructors/transport/middlewares/client"
 	serverMiddlewares "github.com/orchestd/dependencybundler/constructors/transport/middlewares/server"
-	"github.com/orchestd/dependencybundler/depBundler/middlewares/context"
+	middlewaresContext "github.com/orchestd/dependencybundler/depBundler/middlewares/context"
 	"github.com/orchestd/dependencybundler/depBundler/middlewares/metrics"
 	"github.com/orchestd/dependencybundler/depBundler/middlewares/trace"
 	"github.com/orchestd/dependencybundler/interfaces/transport"
+	"github.com/orchestd/servicereply"
 	"github.com/orchestd/transport/client"
 	httpClient "github.com/orchestd/transport/client/http"
 	"github.com/orchestd/transport/server"
@@ -57,7 +61,7 @@ func TransportFxOption(monolithConstructor ...interface{}) fx.Option {
 		fx.Provide(fx.Annotated{Group: ApiInterceptors, Target: serverMiddlewares.DefaultJwtToken}),
 		fx.Provide(fx.Annotated{Group: ApiInterceptors, Target: serverMiddlewares.DefaultLogHandlerMiddleware}),
 		fx.Provide(fx.Annotated{Group: ApiInterceptors, Target: trace.HttpTracingUnaryServerInterceptor}),
-		fx.Provide(fx.Annotated{Group: ApiInterceptors, Target: context.CallerToContext}),
+		fx.Provide(fx.Annotated{Group: ApiInterceptors, Target: middlewaresContext.CallerToContext}),
 		fx.Provide(fx.Annotated{Group: ApiInterceptors, Target: metrics.AverageRequestDurationMetric}),
 
 		fx.Provide(func() client.HTTPClientBuilder {
@@ -67,4 +71,19 @@ func TransportFxOption(monolithConstructor ...interface{}) fx.Option {
 
 		userConstructors,
 	)
+}
+
+func RunMethodWithTrace(c context.Context, serviceName, operationName string, tracer opentracing.Tracer, req interface{}, funcToRun func(con context.Context) (interface{}, servicereply.ServiceReply)) servicereply.ServiceReply {
+	sp, _ := opentracing.StartSpanFromContextWithTracer(c, tracer, serviceName+"/"+operationName)
+	defer sp.Finish()
+	ext.DBStatement.Set(sp, serviceName+"/"+operationName)
+	ext.Component.Set(sp, serviceName)
+	sp.SetTag("request", req)
+	if r, err := funcToRun(c); err != nil {
+		ext.LogError(sp, err.GetError())
+		return err
+	} else {
+		sp.SetTag("response", r)
+	}
+	return nil
 }
