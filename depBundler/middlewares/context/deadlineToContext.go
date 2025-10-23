74bbd8252ffeaa0bd69482aca5b135ee8919422d
andreimikheev <mixe.andrey@gmail.com>
2025-10-23 13:09:10 +0300

    tmp

---
+++ depBundler/middlewares/context/deadlineToContext.go
@@ -0,0 +1,18 @@
+package context
+
+import (
+	"context"
+
+	"github.com/gin-gonic/gin"
+)
+
+// IpToContext middleware adds the client ip to the context
+func DeadlineToContext() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		clientIp := c.ClientIP()
+		ctx := context.WithValue(c.Request.Context(), "clientIp", clientIp)
+		c.Request = c.Request.WithContext(ctx)
+		c.Next()
+		return
+	}
+}
